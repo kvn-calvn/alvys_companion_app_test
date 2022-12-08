8462bf368fccc54c2de291fddcb41a019e18770d
kcalvin <kcalvin@outlook.com>
2022-12-08 12:43:31 -0500

    add profile page

--- lib/src/features/settings/presentation/profile_page.dart
+++ lib/src/features/settings/presentation/profile_page.dart
@@ -35,7 +35,114 @@ class ProfilePage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
-        child: Container(),
+        child: ListView(children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage:
+                    NetworkImage("https://via.placeholder.com/150"),
+                backgroundColor: Colors.transparent,
+              ),
+            ],
+          ),
+          TextButton(onPressed: () {}, child: const Text("Edit Picture")),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Full Name',
+                style: Theme.of(context).textTheme.labelLarge,
+              ),
+              Text(
+                'Sandbox Driver',
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Text(
+                'Email',
+                style: Theme.of(context).textTheme.labelLarge,
+              ),
+              Text(
+                'sandbox@alvys.com',
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Text(
+                'Phone',
+                style: Theme.of(context).textTheme.labelLarge,
+              ),
+              Text(
+                '9094623310',
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Text(
+                'Tenant (s)',
+                style: Theme.of(context).textTheme.labelLarge,
+              ),
+              Text(
+                'Calvin Logistics, Archerhub',
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Text(
+                'Address',
+                style: Theme.of(context).textTheme.labelLarge,
+              ),
+              Text(
+                '123 Melrose St',
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+              Text(
+                'Brooklyn, NY 11206',
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Text(
+                'License #',
+                style: Theme.of(context).textTheme.labelLarge,
+              ),
+              Text(
+                'LC546987',
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Text(
+                'License Expiration Date',
+                style: Theme.of(context).textTheme.labelLarge,
+              ),
+              Text(
+                'Aug 16, 2023',
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Text(
+                'License Issue State',
+                style: Theme.of(context).textTheme.labelLarge,
+              ),
+              Text(
+                '-',
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ],
+          )
+        ]),
       ),
     );
     ;
