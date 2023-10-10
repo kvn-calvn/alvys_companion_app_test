c8f97f4a3b215628faf35691cd0decbfddd21915
kcalvin <kcalvin@outlook.com>
2023-10-09 21:49:49 -0500

    Update trip_card.dart

--- lib/src/common_widgets/trip_card.dart
+++ lib/src/common_widgets/trip_card.dart
@@ -1,4 +1,3 @@
-import 'package:alvys3/src/network/firebase_remote_config_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
