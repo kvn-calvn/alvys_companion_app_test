db3094dc75d33ec3bd5bfca36385231e94a30b40
kcalvin <kcalvin@outlook.com>
2024-01-03 19:57:08 -0500

    Update trip_repository.dart

--- lib/src/features/trips/data/repositories/trip_repository.dart
+++ lib/src/features/trips/data/repositories/trip_repository.dart
@@ -1,5 +1,4 @@
 import 'package:coder_matthews_extensions/coder_matthews_extensions.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../../constants/api_routes.dart';
