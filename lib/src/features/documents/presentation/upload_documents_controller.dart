715ec6f382e8eae34c32351f39f124aa08442c27
Terrence Matthews <matthewsterrence1996@gmail.com>
2024-05-16 14:12:39 -0500

    Update upload_documents_controller.dart

--- lib/src/features/documents/presentation/upload_documents_controller.dart
+++ lib/src/features/documents/presentation/upload_documents_controller.dart
@@ -24,11 +24,9 @@ import '../domain/genius_scan_config/genius_scan_generate_document_config.dart';
 import '../domain/upload_documents_state/upload_documents_state.dart';
 import 'docs_controller.dart';
 
-class UploadDocumentsController
-    extends AutoDisposeFamilyNotifier<UploadDocumentsState, UploadDocumentArgs>
+class UploadDocumentsController extends AutoDisposeFamilyNotifier<UploadDocumentsState, UploadDocumentArgs>
     implements IErrorHandler {
-  late AppDocumentRepository<DocumentsNotifier, UploadDocumentsController>
-      docRepo;
+  late AppDocumentRepository<DocumentsNotifier, UploadDocumentsController> docRepo;
   late TripController trips;
   late DocumentsNotifier docList;
   late ScanningNotifier isScanning;
@@ -42,8 +40,7 @@ class UploadDocumentsController
     userData = ref.read(authProvider.notifier);
     isScanning = ref.read(scanningProvider.notifier);
     router = ref.read(getRouter);
-    docList = ref.read(
-        documentsProvider.call(DocumentsArgs(arg.documentType, null)).notifier);
+    docList = ref.read(documentsProvider.call(DocumentsArgs(arg.documentType, null)).notifier);
     state = UploadDocumentsState(documentType: dropDownOptions.first);
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
       startScan();
@@ -64,16 +61,13 @@ class UploadDocumentsController
         break;
     }
     try {
-      var res = await FlutterGeniusScan.scanWithConfiguration(
-          config.toJson().removeNulls);
+      var res = await FlutterGeniusScan.scanWithConfiguration(config.toJson().removeNulls);
       var results = GeniusScanResults.fromJson(jsonDecode(jsonEncode(res)));
       if (firstScan && results.scans.isEmpty) {
         router.pop();
       }
-      state = state.copyWith(pages: [
-        ...state.pages,
-        ...results.scans.map<String>((e) => Scan.toPathString(e.enhancedUrl!))
-      ]);
+      state = state
+          .copyWith(pages: [...state.pages, ...results.scans.map<String>((e) => Scan.toPathString(e.enhancedUrl!))]);
       firstScan = false;
     } catch (e) {
       if (firstScan) {
@@ -90,11 +84,8 @@ class UploadDocumentsController
     if (state.pages.isEmpty) return;
     var pages = List<String>.from(state.pages);
     pages.removeAt(state.pageNumber);
-    var indexToResetTo = state.pageNumber >= state.pages.length - 1
-        ? state.pageNumber - 1
-        : state.pageNumber;
-    state = state.copyWith(
-        pages: pages, pageNumber: indexToResetTo.clamp(0, pages.length + 1));
+    var indexToResetTo = state.pageNumber >= state.pages.length - 1 ? state.pageNumber - 1 : state.pageNumber;
+    state = state.copyWith(pages: pages, pageNumber: indexToResetTo.clamp(0, pages.length + 1));
   }
 
   void updatePageNumber(int index) {
@@ -112,30 +103,25 @@ class UploadDocumentsController
 
     await FlutterGeniusScan.generateDocument(
         GeniusScanGeneratePDFConfig(
-                pages: state.pages
-                    .map((e) => GeneratePDFPage(
-                        imageUrl: GeneratePDFPage.toPathString(e)))
-                    .toList())
+                pages: state.pages.map((e) => GeneratePDFPage(imageUrl: GeneratePDFPage.toPathString(e))).toList())
             .toJson(),
         {'outputFileUrl': GeneratePDFPage.toPathString(path)});
     var pdfFile = File(path);
-    int fileLimit = 12;
-    ValidationContract.requiresWithCallback(
-        await pdfFile.sizeInMb <= fileLimit,
-        'File Limit Exceeded',
-        'File is over ${fileLimit}mb. Remove some pages if possible and try again',
-        () {
-      onError(Exception());
-    });
+    // int fileLimit = 12;
+    // ValidationContract.requiresWithCallback(
+    //     await pdfFile.sizeInMb <= fileLimit,
+    //     'File Limit Exceeded',
+    //     'File is over ${fileLimit}mb. Remove some pages if possible and try again',
+    //     () {
+    //   onError(Exception());
+    // });
     await _doUpload(pdfFile);
 
     if (context.mounted) {
       Navigator.of(context, rootNavigator: true).pop();
       context.pop();
       if (arg.documentType == DisplayDocumentType.tripDocuments) {
-        ref
-            .read(tripControllerProvider.notifier)
-            .refreshCurrentTrip(arg.tripId!);
+        ref.read(tripControllerProvider.notifier).refreshCurrentTrip(arg.tripId!);
       }
     }
   }
@@ -146,20 +132,15 @@ class UploadDocumentsController
     switch (arg.documentType) {
       case DisplayDocumentType.tripDocuments:
         var trip = trips.getTrip(arg.tripId!);
-        await docRepo.uploadTripDocuments(
-            userData.driver!, state.documentType!, pdfFile, trip!);
+        await docRepo.uploadTripDocuments(userData.driver!, state.documentType!, pdfFile, trip!);
         await trips.refreshCurrentTrip(arg.tripId!);
       case DisplayDocumentType.personalDocuments:
-        await docRepo.uploadPersonalDocuments(
-            userData.getCompanyOwned.companyCode!,
-            state.documentType!,
-            pdfFile);
+        await docRepo.uploadPersonalDocuments(userData.getCompanyOwned.companyCode!, state.documentType!, pdfFile);
         await docList.getDocuments();
       case DisplayDocumentType.paystubs:
         await Future.value(null);
       case DisplayDocumentType.tripReport:
-        await docRepo.uploadTripReport(userData.getCompanyOwned.companyCode!,
-            state.documentType!, pdfFile);
+        await docRepo.uploadTripReport(userData.getCompanyOwned.companyCode!, state.documentType!, pdfFile);
         await docList.getDocuments();
     }
   }
@@ -183,25 +164,21 @@ class UploadDocumentsController
         ], trip?.companyCode);
 
       case DisplayDocumentType.personalDocuments:
-        return UploadDocumentOptions.getOptionsList([
-          (title: "Driver License", data: "Driver License"),
-          (title: "Medical", data: "Medical")
-        ], userData.getCompanyOwned.companyCode!);
+        return UploadDocumentOptions.getOptionsList(
+            [(title: "Driver License", data: "Driver License"), (title: "Medical", data: "Medical")],
+            userData.getCompanyOwned.companyCode!);
       case DisplayDocumentType.paystubs:
         return [];
       case DisplayDocumentType.tripReport:
         return UploadDocumentOptions.getOptionsList(
-            [(title: 'Trip Report', data: 'Trip Report')],
-            userData.getCompanyOwned.companyCode!);
+            [(title: 'Trip Report', data: 'Trip Report')], userData.getCompanyOwned.companyCode!);
     }
   }
 
   @override
   FutureOr<void> onError(Exception ex) {
     if (router.routerDelegate.navigatorKey.currentContext?.mounted ?? false) {
-      Navigator.of(router.routerDelegate.navigatorKey.currentContext!,
-              rootNavigator: true)
-          .pop();
+      Navigator.of(router.routerDelegate.navigatorKey.currentContext!, rootNavigator: true).pop();
     }
   }
 
@@ -209,13 +186,11 @@ class UploadDocumentsController
   FutureOr<void> refreshPage(String page) {}
 }
 
-final uploadDocumentsController = AutoDisposeNotifierProviderFamily<
-    UploadDocumentsController,
-    UploadDocumentsState,
-    UploadDocumentArgs>(UploadDocumentsController.new);
+final uploadDocumentsController =
+    AutoDisposeNotifierProviderFamily<UploadDocumentsController, UploadDocumentsState, UploadDocumentArgs>(
+        UploadDocumentsController.new);
 
-final scanningProvider =
-    NotifierProvider<ScanningNotifier, bool>(ScanningNotifier.new);
+final scanningProvider = NotifierProvider<ScanningNotifier, bool>(ScanningNotifier.new);
 
 class ScanningNotifier extends Notifier<bool> {
   @override
@@ -230,20 +205,14 @@ class UploadDocumentArgs {
   final UploadType uploadType;
   final DisplayDocumentType documentType;
   final String? tripId;
-  UploadDocumentArgs(
-      {this.tripId, required this.uploadType, required this.documentType});
+  UploadDocumentArgs({this.tripId, required this.uploadType, required this.documentType});
 }
 
 class UploadDocumentOptions {
   final String companyCode, title, value;
 
-  UploadDocumentOptions(
-      {required this.companyCode, required this.title, required this.value});
+  UploadDocumentOptions({required this.companyCode, required this.title, required this.value});
 
-  static List<UploadDocumentOptions> getOptionsList(
-          List<({String title, String data})> titles, String? companyCode) =>
-      titles
-          .map((e) => UploadDocumentOptions(
-              companyCode: companyCode ?? "", title: e.title, value: e.data))
-          .toList();
+  static List<UploadDocumentOptions> getOptionsList(List<({String title, String data})> titles, String? companyCode) =>
+      titles.map((e) => UploadDocumentOptions(companyCode: companyCode ?? "", title: e.title, value: e.data)).toList();
 }
