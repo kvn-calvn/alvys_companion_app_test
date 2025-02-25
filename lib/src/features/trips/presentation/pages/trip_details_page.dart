e773323caf9b8dc374649283543f234f00d7ad42
Bryan Jorge <bryan.jorge@alvys.com>
2025-02-25 17:27:09 +0000

    Merged PR 41162: Fix tabs
    - Remove unused extension import from trip_details_page.dart
    - Refactor user identification logic in LoadListPage to use userValue directly

--- lib/src/features/trips/presentation/pages/trip_details_page.dart
+++ lib/src/features/trips/presentation/pages/trip_details_page.dart
@@ -12,7 +12,6 @@ import '../../../../network/firebase_remote_config_service.dart';
 import '../../../../network/http_client.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 
-import '../../../../utils/extensions.dart';
 import '../../../../utils/tablet_utils.dart';
 
 import '../../../google_maps_helper/presentation/trip_google_map.dart';
@@ -40,39 +39,46 @@ class LoadDetailsPage extends ConsumerStatefulWidget {
 }
 
 class _LoadDetailsPageState extends ConsumerState<LoadDetailsPage> with TickerProviderStateMixin {
-  // late TabController _tabController;
+  late TabController _tabController;
   TripController get tripController => ref.read(tripControllerProvider.notifier);
   AppTrip? get tripState => ref.read(tripControllerProvider).value?.getTrip(widget.tripId);
 
   @override
   void initState() {
     super.initState();
-    TabletUtils.instance.detailsController =
-        TabController(initialIndex: widget.tabIndex, length: 3, vsync: this);
-    TabletUtils.instance.detailsController.addListener(tabListener);
+    _tabController = TabController(initialIndex: widget.tabIndex, length: 3, vsync: this);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _tabController.addListener(tabListener);
+      TabletUtils.instance.detailsController = _tabController;
+    });
   }
 
   @override
   void dispose() {
-    TabletUtils.instance.detailsController.removeListener(tabListener);
-    TabletUtils.instance.detailsController.dispose();
+    _tabController.removeListener(tabListener);
+    _tabController.dispose();
     super.dispose();
   }
 
   void tabListener() {
-    var tabC = TabletUtils.instance.detailsController;
+    var tabC = _tabController;
     if (tabC.index == tabC.previousIndex || tabC.indexIsChanging) return;
     ref.read(postHogProvider).postHogScreen(
-        tripController.tabName(TabletUtils.instance.detailsController.index),
-        tripState?.loadNumber == null ? null : {'LoadNumber': tripState?.loadNumber ?? ''});
+          tripController.tabName(_tabController.index),
+          tripState?.loadNumber == null ? null : {'LoadNumber': tripState?.loadNumber ?? ''},
+        );
   }
 
   @override
   Widget build(BuildContext context) {
-    var trip = ref.watch(tripControllerProvider).value!.getTrip(widget.tripId);
-    var showTutBtn = ref.watch(firebaseRemoteConfigServiceProvider).showTutorialBtn();
-    // ref.read(postHogProvider).postHogScreen('Trip Details - ${trip.loadNumber ?? ''}',
-    //     {'load_number': trip.loadNumber ?? '', 'trip_id': trip.id ?? ''});
+    var tripDetailsState = ref.watch(tripControllerProvider);
+    var trip = tripDetailsState.value?.getTrip(widget.tripId);
+    var showTutorialBtn = ref.watch(firebaseRemoteConfigServiceProvider).showTutorialBtn();
+
+    if (tripDetailsState.isLoading) return const TripDetailsShimmer();
+    if (trip == null) {
+      return const EmptyView(title: 'Trip Not found', description: 'Return to the previous page');
+    }
 
     return Scaffold(
       appBar: AppBar(
@@ -90,29 +96,22 @@ class _LoadDetailsPageState extends ConsumerState<LoadDetailsPage> with TickerPr
           },
         ),
         actions: [
-          if (showTutBtn) ...[
+          if (showTutorialBtn) ...[
             IconButton(
               constraints: const BoxConstraints(),
               onPressed: () async {
                 var previousTripId = widget.tripId;
                 await ref.read(tripControllerProvider.notifier).showTripDetailsTutorialPreview(
-                    context,
-                    TabletUtils.instance.detailsController.index + 1,
-                    TabletUtils.instance.detailsController.index + 1,
-                    previousTripId);
+                      context,
+                      _tabController.index + 1,
+                      _tabController.index + 1,
+                      previousTripId,
+                    );
                 ref
                     .read(httpClientProvider)
                     .telemetryClient
                     .trackEvent(name: "trip_details_tour_button_tapped");
                 await FirebaseAnalytics.instance.logEvent(name: "trip_details_tour_button_tapped");
-                // context.goNamed(RouteName.tripDetails.name, pathParameters: {ParamType.tripId.name: testTrip.id!});
-                // ref.read(tripControllerProvider.notifier).prepareTutorialPreview(context,
-                //     TabletUtils.instance.detailsController.index + 1, TabletUtils.instance.detailsController.index + 1);
-                // ref.read(tutorialProvider).showTutorialSection(context, TabletUtils.instance.detailsController.index + 1,
-                //     TabletUtils.instance.detailsController.index + 1, () async {
-                //   context.goNamed(RouteName.tripDetails.name, pathParameters: {ParamType.tripId.name: previousTripId});
-                //   // await ref.read(tripControllerProvider.notifier).refreshTrips(true);
-                // });
               },
               icon: const Icon(Icons.info),
             ),
@@ -141,7 +140,7 @@ class _LoadDetailsPageState extends ConsumerState<LoadDetailsPage> with TickerPr
         backgroundColor: Colors.transparent,
         elevation: 0,
         bottom: TabBar(
-          controller: TabletUtils.instance.detailsController,
+          controller: _tabController,
           labelStyle: Theme.of(context).textTheme.bodyLarge,
           tabs: <Widget>[
             Consumer(builder: (context, ref, child) {
@@ -165,12 +164,14 @@ class _LoadDetailsPageState extends ConsumerState<LoadDetailsPage> with TickerPr
           ],
         ),
       ),
-      body: TabBarView(controller: TabletUtils.instance.detailsController, children: [
-        TripDetails(widget.tripId, widget.tabIndex),
-        EcheckPage(widget.tripId),
-        TripDocuments(widget.tripId)
-        // DocumentsPage(DocumentsArgs(DocumentType.tripDocuments, widget.tripId)),
-      ]),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          TripDetails(widget.tripId, widget.tabIndex),
+          EcheckPage(widget.tripId),
+          TripDocuments(widget.tripId)
+        ],
+      ),
     );
   }
 }
@@ -183,7 +184,7 @@ class TripDetails extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final tripDetailsState = ref.watch(tripControllerProvider);
-    var trip = tripDetailsState.value!.tryGetTrip(tripId);
+    var trip = tripDetailsState.value?.tryGetTrip(tripId);
     if (tripDetailsState.isLoading) return const TripDetailsShimmer();
     if (trip == null) {
       return const EmptyView(title: 'Trip Not found', description: 'Return to the previous page');
@@ -195,105 +196,49 @@ class TripDetails extends ConsumerWidget {
         if (context.mounted) ref.read(websocketProvider).restartConnection();
       },
       child: ListView(
-          scrollDirection: Axis.vertical,
-          controller: ref.read(tutorialProvider).tripInfoScrollController,
-          padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 20),
-          children: [
-            Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    TripGoogleMap(key: UniqueKey(), trip.id!),
-                    // Column(
-                    //   mainAxisSize: MainAxisSize.max,
-                    //   mainAxisAlignment: MainAxisAlignment.center,
-                    //   children: [
-                    //     Padding(
-                    //       padding: const EdgeInsetsDirectional.fromSTEB(0, 10, 15, 0),
-                    //       child: Wrap(
-                    //         direction: Axis.horizontal,
-                    //         children: [
-                    //           if (equipment.isNotNullOrEmpty) ...[
-                    //             ChipCard(text: equipment),
-                    //           ],
-                    //           if (trip.temperature != null) ...[
-                    //             ChipCard(
-                    //                 text:
-                    //                     '${trip.temperature!.toStringAsFixed(1)} °f ${(trip.continuous ?? false) ? '(cont.)' : ''}')
-                    //           ],
-                    //           if (trip.trailerNum.isNotNullOrEmpty) ...[
-                    //             ChipCard(text: 'Trailer ${trip.trailerNum}'),
-                    //           ],
-                    //           if (trip.totalWeight != null && trip.totalWeight != 0) ...[
-                    //             ChipCard(text: '${NumberFormat.decimalPattern().format(trip.totalWeight)} lbs'),
-                    //           ],
-                    //           if (trip.totalMiles != null) ...[
-                    //             ChipCard(text: '${NumberFormat.decimalPattern().format(trip.totalMiles)} mi'),
-                    //           ],
-                    //           if (trip.driverPayable(authState.value!.tryGetUserTenant(trip.companyCode!)?.assetId) !=
-                    //                   null &&
-                    //               authState.value!.shouldShowPayableAmount(trip.companyCode!)) ...[
-                    //             ChipCard(
-                    //               text:
-                    //                   'Driver Payable ${NumberFormat.simpleCurrency().format(trip.driverPayable(authState.value!.tryGetUserTenant(trip.companyCode!)!.assetId!))}',
-                    //             ),
-                    //           ],
-                    //         ],
-                    //       ),
-                    //     ),
-                    //   ],
-                    // ),
-                    const SizedBox(height: 16),
-                    TripDetailsInfo(trip),
-                    const SizedBox(height: 8),
-                    TripReferencesCard(
-                      key: ref.read(tutorialProvider).tripReferencesCard,
-                      tripReferences: trip.loadReferences,
-                      tripId: tripId,
-                      tabIndex: tabIndex,
-                    ),
-                    if (trip.stops.isNullOrEmpty) ...{
-                      Center(
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            const SizedBox(
-                              height: 100,
-                            ),
-                            Text(
-                              "No Stops",
-                              style: Theme.of(context).textTheme.bodyLarge,
-                            ),
-                            Text("There are no stops on this trip.",
-                                style: Theme.of(context).textTheme.bodyMedium)
-                          ],
-                        ),
-                      ),
-                    } else ...{
-                      Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          ...trip.stops.mapList((stop, index, last) => StopCard(
-                                index: index,
-                                stop: stop,
-                                tripId: trip.id!,
-                                tabIndex: tabIndex,
-                                tripNumber: trip.tripNumber!,
-                                canCheckInOutStopId: trip.canCheckInOutStopId,
-                              ))
-                        ],
-                      ),
-                    }
-                  ],
-                ),
-              ],
+        controller: ref.read(tutorialProvider).tripInfoScrollController,
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+        children: [
+          TripGoogleMap(key: UniqueKey(), trip.id!),
+          const SizedBox(height: 16),
+          TripDetailsInfo(trip),
+          const SizedBox(height: 8),
+          TripReferencesCard(
+            key: ref.read(tutorialProvider).tripReferencesCard,
+            tripReferences: trip.loadReferences,
+            tripId: tripId,
+            tabIndex: tabIndex,
+          ),
+          if (trip.stops.isNullOrEmpty)
+            SizedBox(
+              height: 100,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "No Stops",
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                  Text("There are no stops on this trip.",
+                      style: Theme.of(context).textTheme.bodyMedium)
+                ],
+              ),
             )
-          ]),
+          else ...{
+            for (var i = 0; i < trip.stops.length; i++) ...{
+              StopCard(
+                index: i,
+                stop: trip.stops[i],
+                tripId: trip.id!,
+                tabIndex: tabIndex,
+                tripNumber: trip.tripNumber!,
+                canCheckInOutStopId: trip.canCheckInOutStopId,
+              ),
+            },
+          }
+        ],
+      ),
     );
   }
 }
